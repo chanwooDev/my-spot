075532575ac391d85c6091de5916a6f0c21c1aa4
HyeonJunSong <shj9801@naver.com>
2022-06-08 17:42:08 +0900

    initial commit

---
+++ MySpot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySpot
+//
+//  Created by 송현준 on 2022/05/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MySpot/MySpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpotApp.swift
+//  MySpot
+//
+//  Created by 송현준 on 2022/05/11.
+//
+
+import SwiftUI
+
+@main
+struct MySpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
